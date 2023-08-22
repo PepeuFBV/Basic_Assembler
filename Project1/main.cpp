a312e99247e49575e311586c5a2ffef656b853a0
Pedro <pedrofigueiraboaviagem@gmail.com>
2023-08-21 23:50:59 -0300

    Conserto das instruções iniciais
    Corrigida a primeira leitura do programa, que apenas encontra os labels e linha em quais estão. Também começada a segunda leitura, que realmente analisa os comandos.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,7 +2,6 @@
 #include <unordered_map>
 #include <fstream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
@@ -13,38 +12,47 @@ void organizaRegistradores(string opcode, string function, string rd, string rs,
 //ler uma segunda vez apenas o código e se label for referenciado olhar a array e definir
 
 int main() {
+
     ifstream file("entrada.asm");
     if (!file.is_open() && !file.bad()) {
-        cerr << "Could not open the file.\n";
+        cerr << "Arquivo não pode ser aberto\n"; //exibe a mensagem de erro
         return 1;
     }
 
-    string linha;
-    vector<string> labels;
+    string linhaAtual;
+    unordered_map<string, int> labels;
+    int nLinha = 0;
 
-    while (getline(file, linha)) { //le a linha atual
-        size_t pos = linha.find(':'); //encontra o ':' na linha
-        if (pos != string::npos) {
-            string label = linha.substr(0, pos); //obtem o label sem o resto da string
-            labels.push_back(label); //coloca a label 
+    while (getline(file, linhaAtual)) { //loop que lê de linha em linha
+        nLinha++;
+
+        size_t indice = linhaAtual.find(':'); //encontra indice de ':'
+        if (indice != string::npos) { //verifica se ':' foi encontrado, se não, apenas seguir lendo as linhas
+            string label = linhaAtual.substr(0, indice); //cria uma nova substring cortando a partir do início até o índice - 1
+            labels[label] = nLinha; //salva no map com o label e linha (virará endereço) do comando
         }
+
+    }
+    file.close(); //le o arquivo pela primeira vez pegando os labels
+
+
+    file.open("entrada.asm"); //abre o arquivo pela segunda vez para ai realmente transformar em binário
+    if (!file.is_open() && !file.bad()) {
+        cerr << "Arquivo não pode ser aberto\n"; //exibe a mensagem de erro
+        return 1;
     }
 
-    file.close();
+    while (getline(file, linhaAtual)) { //loop que lê de linha em linha
 
-    // Print the extracted labels
-    cout << "Extracted labels:\n";
-    for (const string& label : labels) {
-        cout << label << endl;
+        size_t indice = linhaAtual.find(':'); //encontra indice de ':'
+        if (indice != string::npos) { //verifica se ':' foi encontrado, se não, apenas seguir lendo as linhas
+            //cria uma nova substring cortando a partir do fim do label até o final da string completa
+            linhaAtual = linhaAtual.substr(indice+2, linhaAtual.length());
+        }
+        cout << linhaAtual;
+        //temos que ver ainda a questão de remover os /t das strings após a remoção dos labels
+        //em sequencia ver qual é a instrução, seguindo para um switch para cada tipo de instrução
     }
-
-
-
-
-
-
-
-
     
 
 
